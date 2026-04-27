b64d8f82f9e6ef2bbafbf6f20f121ebfc1a59455
ttmcswg <ttmcswg.po@gmail.com>
2026-04-27 16:20:12 +0300

    test: setup jest and supertest testing infrastructure

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  clearMocks: true,
+};
+
+export default config;
